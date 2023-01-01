93698e4b7e27237c09b059f3c9d663e12cb537e4
Salama-Naief <eng.salama.naief@gmail.com>
2023-01-01 15:49:34 +0200

    database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,13 @@
-const { parse } = require("pg-connection-string");
-
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
   return {
     connection: {
       client: "postgres",
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
+        host: env("DATABASE_HOST"),
+        port: env("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        user: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       debug: false,
     },
