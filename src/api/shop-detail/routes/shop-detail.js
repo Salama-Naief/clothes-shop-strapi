d891d26fc933542dbe68ace2b5a24fdca1092531
Salama-Naief <eng.salama.naief@gmail.com>
2023-01-01 10:36:12 +0200

    initial commit

---
+++ src/api/shop-detail/routes/shop-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shop-detail router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::shop-detail.shop-detail');
---
+++ src/api/shop-detail/services/shop-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shop-detail service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::shop-detail.shop-detail');
