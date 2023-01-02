53e38e53ccdbdaf7b6dcd69a9b167fd271a0f35b
Salama-Naief <eng.salama.naief@gmail.com>
2023-01-02 21:59:29 +0200

    update1

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -6,4 +6,14 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::order.order");
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { data } = ctx.request.body;
+
+    const response = await strapi.entityService.create("api::order.order", {
+      data: data,
+    });
+
+    return { response };
+  },
+}));
